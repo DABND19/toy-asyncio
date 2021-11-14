d5b67f5af4e2ae71ca337150591450522bb96f0d
DABND <grigorenkodavid@gmail.com>
2021-11-14 20:54:03 +0300

    Add Future class.

---
+++ future.py
@@ -0,0 +1,36 @@
+import logging
+
+
+class Future:
+    def __init__(self) -> None:
+        self.result = None
+        self.done = False
+        self.done_callbacks = set()
+        self.exception = None
+
+    def _handle_callbacks(self):
+        for callback in self.done_callbacks:
+            try:
+                callback(self)
+            except Exception:
+                logging.exception(f'Unhandled exception in {callback}')
+
+    def set_result(self, result):
+        self.done = True
+        self.result = result
+        self._handle_callbacks()
+
+    def set_exception(self, exception):
+        self.done = True
+        self.exception = exception
+        self._handle_callbacks()
+
+    def get_result(self):
+        if self.exception:
+            raise self.exception
+        return self.result
+
+    def __iter__(self):
+        while not self.done:
+            yield None
+        return self.get_result()
